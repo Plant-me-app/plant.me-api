3c157969aff2e677e0e165f3e1aea0004384f6a7
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2023-01-03 23:25:14 -0300

    Change plant model to have new attributes
    #6
    
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>

--- src/models/plant.js
+++ src/models/plant.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
- 
+
 const plantSchema = new Schema({
   name: String,
+  species: String,
+  size: String,
+  image: String
 });
  
 module.exports = mongoose.model("Plant", plantSchema);
